6be78a92802a9a63c5cfa9ff25e09aee9fba6d76
Rober <oliroberto@gmail.com>
2015-07-29 21:17:40 +0200

    Errores

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,8 @@ router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new', quizController.new);
-router.get('/quizes/create', quizController.create);
+router.post('/quizes/create', quizController.create);
+
 router.get('/author', function(req,res) {
   res.render('author', {});
 });
