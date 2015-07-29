92c0e5342fd69cc09afdf18847f6e456946b1086
Rober <oliroberto@gmail.com>
2015-07-29 21:54:13 +0200

    Errores solventados

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,6 +45,7 @@ exports.new = function (req, res) {
 //POST /quizes/create
 exports.create = function (req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
+
   quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
     res.redirect('/quizes');
   })
