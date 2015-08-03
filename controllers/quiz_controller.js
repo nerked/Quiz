715e524c83c7b75d2cd1c7ff30edbddcb21494e6
Rober <oliroberto@gmail.com>
2015-08-03 13:01:20 +0200

    Modulo 8 P2P Obligatorio, Añadir y Editar el campo Tema

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -38,7 +38,7 @@ exports.answer = function(req,res){
 //GET /quizes/new
 exports.new = function (req, res) {
   var quiz = models.Quiz.build(
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema:"Tema"}
   );
   res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -55,7 +55,7 @@ exports.create = function (req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       }else{
         quiz
-        .save({fields: ["pregunta", "respuesta"]})
+        .save({fields: ["pregunta", "respuesta", "tema"]})
         .then(function() { res.redirect('/quizes')})
       }
     }
@@ -82,7 +82,7 @@ exports.update = function(req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
+        .save( {fields: ["pregunta", "respuesta","tema"]})
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
