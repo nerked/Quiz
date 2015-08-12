eb9d752f41e9ad5c8d9fcac4154d6fdc5d42db98
Rober <oliroberto@gmail.com>
2015-08-12 19:04:58 +0200

    Comentarios añadidos

---
+++ controllers/comment_controller.js
@@ -0,0 +1,33 @@
+var models = require('../models/models.js');
+
+//GET /quizes/:quizId/comments/new
+exports.new = function(req,res){
+	res.render('comments/new.ejs', { quizId: req.params.quizId, errors: [] });
+};
+
+//POST /quizes/:quizId/comments
+exports.create = function(req,res){
+	var comment = models.Comment.build(
+    {
+      texto: req.body.comment.texto,
+      QuizId: req.params.quizId
+    });
+
+	comment
+	.validate()
+	.then(
+    function(error){
+      if(error){
+        res.render('comments/new.ejs',{
+          comment: comment,
+          quizId: req.params.quizId,
+				  errors: error.errors
+			  });
+      }else{
+        comment
+        .save()
+			  .then(function(){res.redirect('/quizes/' + req.params.quizId)})
+		  }
+    }
+  ).catch(function(error){next(error)});
+};
